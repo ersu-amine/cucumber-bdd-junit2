a97308790417d53b5393a0052d4ec872f6960a54
ersu-amine <ersu-amine@gmail.com>
2024-12-07 23:58:18 -0500

    feat(basepage): add methods and web elements for reusable functionality
    Implemented new utility methods and added web elements to the BasePage to enhance reusability and simplify page interactions.

--- src/test/java/com/bdd/pages/saucedemo/BasePage.java
+++ src/test/java/com/bdd/pages/saucedemo/BasePage.java
@@ -1,6 +1,9 @@
 package com.bdd.pages.saucedemo;
 
 import com.bdd.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
@@ -9,4 +12,24 @@ public abstract class BasePage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    @FindBy(id="react-burger-menu-btn")
+    private WebElement hamburgerMenu;
+
+    @FindBy(id = "logout_sidebar_link")
+    private WebElement logoutLink;
+
+    @FindBy(name="login-button")
+    private WebElement loginButton;
+
+    public void clickMenu(){
+        hamburgerMenu.click();
+    }
+
+    public void clickLogoutLink(){
+        logoutLink.click();
+    }
+
+    public void confirmLoginButtonVisibility(){
+        Assert.assertTrue("Login button is not visible",loginButton.isDisplayed());
+    }
 }
