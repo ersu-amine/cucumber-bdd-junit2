184c3222fa01eb14bccabe5f4b2fd3da979d302c
ersu-amine <ersu-amine@gmail.com>
2025-01-13 02:32:51 -0500

    chore(): update runners

--- src/test/java/com/bdd/runners/FailedTestRunner.java
+++ src/test/java/com/bdd/runners/FailedTestRunner.java
@@ -7,10 +7,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {
-        "pretty",
-        "me.jvt.cucumber.report.PrettyReports:target/cucumber"
-        },
         features = "@target/rerun.txt",
         glue = "com/bdd/step_definitions"
 )
